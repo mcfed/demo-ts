745252532183c3b51927cc6ed46d45e13712e93f
lujianan <--add>
2020-05-20 13:56:08 +0800

    add some test

--- e2e/integration/demo.spec.js
+++ e2e/integration/demo.spec.js
@@ -2,13 +2,17 @@ import { namespace } from "../../src/model";
 
 context("Actions", () => {
   beforeEach(() => {
-    cy.visit('e2e/demo.html')
+    cy.visit("e2e/demo.html");
   });
   it("test", () => {
-    cy.get("#test").should("contain", "test cypress");
+    cy.get("#test").should("be.visible").should('have.class', 'test-class');
+    cy.get("#test").should('have.text', 'test cypress');
+    cy.get('#test').should(($div) => {
+      const text = $div.text()
+      expect(text).include(' ')
+    })
   });
 });
-
 
 context.skip("Actions", () => {
   beforeEach(() => {
